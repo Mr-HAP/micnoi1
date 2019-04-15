003fdb9388de401d0406057e41da6d067a92991b
Mr-HAP <sadasx@gmail.com>
2019-04-15 11:01:40 +0200

    Add Seeder: - Users

---
+++ src/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 3)->create();
+        App\User::create([
+            'name' => 'hernan',
+            'email' => 'hernan@hap.cl',
+            'password' => bcrypt('123'),
+        ]);
+    }
+}
