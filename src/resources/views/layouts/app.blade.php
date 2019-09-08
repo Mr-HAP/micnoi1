352ce29918eb62154aa421e934fbc9ddbaa37ded
Mr-HAP <sadasx@gmail.com>
2019-09-08 11:06:07 +0200

    My Offers Menu register

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -21,62 +21,12 @@
 </head>
 <body class="container-fluid">
 <div id="app" class="container">
-    {{--<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>--}}
-    <div class="nav justify-content-end">
+     <div class="nav justify-content-end">
         <nav>
             <!-- Authentication Links -->
             @guest
-                <a class="btn btn-xl accentColor" href="{{ route('login') }}" data-toggle="modal"
-                   data-target="#loginModal" data-whatever="@mdo">{{ __('Login') }}</a>
+                <a class="btn btn-xl accentColor" href="{{ route('login') }}" data-toggle="modal" data-target="#loginModal" data-whatever="@mdo">{{ __('Login') }}</a>
+                <a class="btn btn-xl accentColor" href="{{ route('register') }}" data-whatever="@mdo">{{ __('Register') }}</a>
                 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel"
                      aria-hidden="true">
                     <div class="modal-dialog" role="document">
@@ -163,9 +113,9 @@
                     {{ Auth::user()->name }}
                 </a>
                 <div class="dropdown-menu dropdown-menu-top" aria-labelledby="dropdownMenuOffset">
-                    <a class="dropdown-item" href="#">My Perfil</a>
-                    <a class="dropdown-item" href="#">My Offers</a>
-                    <a class="dropdown-item" href="#">My Bands/Musicxs</a>
+                    <a class="dropdown-item" href="/profile-musician/{{ Auth::user()->id }}">My Perfil</a>
+                    <a class="dropdown-item" href="/my-offers">My Offers</a>
+                    <a class="dropdown-item" href="/my-bands">My Bands/Musicxs</a>
                 </div>
                 <a class="ml-3 text-dark" href="{{ route('logout') }}"
                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
