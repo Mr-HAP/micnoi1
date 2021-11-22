0fba21265aaa76d7408b141e111591f65d308efc
es_7eban@hotmail.com <esteban@digidip.net>
2021-11-22 15:13:58 +0100

    offer API list

---
+++ src/app/Http/Controllers/Api/OfferController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Offer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class OfferController
+{
+    private $genders = [
+        'femenino' => 'Femenino',
+        'masculino' => 'Masculino',
+        'lgbti' => 'LGBTI'
+    ];
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $offers = Offer::all();
+        if ($request->input() !== null) {
+            $offers = $this->filter($offers, $request);
+        }
+
+        return response($offers);
+    }
+    /**
+     * Display a listing offers by country/state.
+     *
+     * @param $offers
+     * @param $request
+     * @return Collection
+     */
+    private function filter($offers, $request)
+    {
+        $filtered = $offers;
+        if ($request->get('state') !== null && $request->get('state') !== '') {
+            $filtered = $offers->where('state_id', '=', $request->get('state'));
+        }
+        if ($request->get('type') !== null && $request->get('type') !== '') {
+            $filtered = $filtered->where('type', '=', $request->get('type'));
+        }
+
+        return $filtered;
+    }
+}
